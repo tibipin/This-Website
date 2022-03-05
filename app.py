32f4a9c6dd7289c611de9b7586cab0aa06383a29
Tiberiu Pintilie <tibipin7@gmail.com>
2022-03-05 19:43:40 +0200

    added ORM and database

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from . import orm
+
+import os
 
 app = Flask(__name__)
+
+# ======================================================
+# ==> set base directory based on location of app.py <==
+# ======================================================
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# =================================================================================
+# ==> configure sqlite as database engine and save the database file in basedir <==
+# =================================================================================
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "planets.db")}'
+
+# ================================
+# ==> intializing the database <==
+# ================================
+db = SQLAlchemy(app)
+
+
 
 
 @app.route('/')

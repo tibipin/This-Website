446c6fe61414f1833821cbde0eee2241be3918e4
Tiberiu Pintilie <tiberiupintilie@Tiberius-MacBook-Air.local>
2022-03-18 20:13:26 +0200

    changed database URI

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@ import os
 
 app = Flask(__name__)
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir,'super_database.db')}"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///super_database.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
